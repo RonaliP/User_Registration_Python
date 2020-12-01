7748d304f5f22b331b88f0f36f6f9b33a803b311
Ronali <ronalipanigrahy88@gmail.com>
2020-12-01 11:32:34 +0530

    Validation for last name added

--- Regex.py
+++ Regex.py
@@ -11,8 +11,23 @@ class RegularExpression():
             return "Valid Firstname"
         else:
             return "Invalid Firstname,Try again"
+
+    def Lastnamecheck(self, last_name):
+        pattern = "^[A-Z]{1}[a-z]{2,}$"
+        if re.match(pattern, last_name):
+            return "Valid Last name"
+        else:
+            return "Invalid Last name"
+
+"""
+Above function will validate last name
+2nd use case
+"""
 if __name__ == '__main__':
     Regex=RegularExpression()
     print("Enter Firstname")
     First_name=input()
     print(Regex.Firstnamecheck(First_name))
+    print("Enter Last name")
+    Last_name = input()
+    print(Regex.Lastnamecheck(Last_name))
