833bae3540585e477bc74e69fc46719665870c9b
Ronali <ronalipanigrahy88@gmail.com>
2020-12-01 11:25:05 +0530

    code to validate firstname added

--- Regex.py
+++ Regex.py
@@ -1,1 +1,18 @@
-print("Welcome to User registration program")
+import re
+"""
+Below is the code to validate first name entered by user
+First use case
+"""
+class RegularExpression():
+    print("Welcome to User registration program")
+    def Firstnamecheck(self,first_name):
+        pattern="^[A-Z]{1}[a-z]{2,}$"
+        if re.match(pattern,first_name):
+            return "Valid Firstname"
+        else:
+            return "Invalid Firstname,Try again"
+if __name__ == '__main__':
+    Regex=RegularExpression()
+    print("Enter Firstname")
+    First_name=input()
+    print(Regex.Firstnamecheck(First_name))
